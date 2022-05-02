530a14a3958fd5e8a5ac4e5098ea79cc4b817ad9
virsinghh <103216400+virsinghh@users.noreply.github.com>
2022-05-02 16:14:14 -0700

    added test

--- skillDemo.java
+++ skillDemo.java
@@ -1,3 +1,7 @@
 public class skillDemo{
 
+    public static int add2(){
+        return 1+1;
+    }
+    
 }
---
+++ skillDemoTester.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skillDemoTester {
+    
+    @Test
+    public void addTest(){
+        assertEquals(3, skillDemo.add2());
+    }
+}
